515128d3e2c3ac2bc78aaa22e81c4054b4f1a606
Chrispinu Melkiory <chrismelky05@gmail.com>
2021-08-20 11:46:48 +0300

    merge

--- src/app/setup/decision-level/decision-level.model.ts
+++
@@ -1,9 +0,0 @@
-export class DecisionLevel {
-  constructor(
-    public id?: number,
-    public name?: string,
-    public admin_hierarchy_position_id?: number,
-    public section_level_id?: number,
-    public next_decision_level_ids?: string
-  ) {}
-}

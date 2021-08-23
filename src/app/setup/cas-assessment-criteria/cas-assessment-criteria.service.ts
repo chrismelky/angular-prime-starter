f23d577952ec39cf8b2f8d7e71c35675bc4b1dc3
jmazigo <jmazigo@gmail.com>
2021-08-23 12:00:39 +0300

    api fixes

--- src/app/setup/cas-assessment-criteria/cas-assessment-criteria.service.ts
+++ src/app/setup/cas-assessment-criteria/cas-assessment-criteria.service.ts
@@ -15,7 +15,7 @@ import { CasAssessmentCriteria } from "./cas-assessment-criteria.model";
 
 @Injectable({ providedIn: "root" })
 export class CasAssessmentCriteriaService {
-  public resourceUrl = "api/criteria";
+  public resourceUrl = "api/criterias";
 
   constructor(protected http: HttpClient) {}
 

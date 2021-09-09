b29f01b553d3f69173dba53c2ba15d3b2c006956
msigala <selemani.msigala@tamisemi.go.tz>
2021-09-09 16:39:53 +0300

    ceilig

--- src/app/setup/admin-hierarchy-ceiling/update/initiate-ceiling.component.ts
+++ src/app/setup/admin-hierarchy-ceiling/update/initiate-ceiling.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {FundSourceBudgetClassService} from "../../fund-source-budget-class/fund-source-budget-class.service";
+import {CustomResponse} from "../../../utils/custom-response";
+import {Section} from "../../section/section.model";
+import {FundSourceBudgetClass} from "../../fund-source-budget-class/fund-source-budget-class.model";
+import {DynamicDialogRef} from "primeng/dynamicdialog";
 
 @Component({
   selector: 'app-initiate-ceiling',
@@ -6,10 +11,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./initiate-ceiling.component.scss']
 })
 export class InitiateCeilingComponent implements OnInit {
-
-  constructor() { }
+  ceilings?: any[] = [];
+  selectedSource: any[]=[];
+  constructor(
+    private fundSourceBudgetClassService:FundSourceBudgetClassService,
+    public dialogRef: DynamicDialogRef,
+  ) { }
 
   ngOnInit(): void {
+    this.fundSourceBudgetClassService
+      .queryProjectionCeiling({ can_project :true,page:1})
+      .subscribe(
+        (resp: CustomResponse<Section[]>) => (this.ceilings = resp.data??[])
+      )
+  }
+  initiate(raw: FundSourceBudgetClass): void{
+    this.selectedSource.push(raw);
+    this.ceilings = this.ceilings!.filter(obj => obj !== raw);
+
+  }
+
+  save(): void{
+
+  }
+
+  close(): void{
+    this.dialogRef.close(true);
   }
 
 }

e0ff0533752f05eed7830164a3d59a69a1737361
Msigala <selemani.msigala@tamisemi.go.tz>
2021-10-07 17:00:23 +0300

    ceiling

--- src/app/budgeting/admin-hierarchy-ceiling/update/admin-ceiling-dissemination.component.ts
+++ src/app/budgeting/admin-hierarchy-ceiling/update/admin-ceiling-dissemination.component.ts
@@ -11,6 +11,7 @@ import {FinancialYear} from "../../../setup/financial-year/financial-year.model"
 import {CeilingChain} from "../../../setup/ceiling-chain/ceiling-chain.model";
 import {Section} from "../../../setup/section/section.model";
 import {SectionService} from "../../../setup/section/section.service";
+import {concatAll} from "rxjs/operators";
 
 @Component({
   selector: 'app-admin-ceiling-dissemination',
@@ -20,10 +21,17 @@ import {SectionService} from "../../../setup/section/section.service";
 export class AdminCeilingDisseminationComponent implements OnInit {
   position?: number;
   section_id?: number;
+  section_level_position?: number;
+  selectedCostCenter?: number;
+  selectedDepartment?: number;
   ceilingStartPosition?:number;
   ceiling?: any=null;
   ceilingChain?: CeilingChain[]=[];
-  department?: any[]=[];
+  departments?: any[]=[];
+  costCenter?: any[]=[];
+
+  totalDepartmentAmount = 0.00;
+  totalCostCenterAmount = 0.00;
 
   /**
    * Declare form
@@ -49,6 +57,7 @@ export class AdminCeilingDisseminationComponent implements OnInit {
     this.position = this.config.data.position;
     this.ceilingStartPosition = this.config.data.ceilingStartPosition;
     this.section_id = this.config.data.section_id;
+    this.section_level_position = this.config.data.section_level_position
 
   }
 
@@ -58,22 +67,67 @@ export class AdminCeilingDisseminationComponent implements OnInit {
       .subscribe(
         (resp: CustomResponse<CeilingChain[]>) =>{
           this.ceilingChain = (resp.data??[]).filter(chain => chain.section_level_position !== 1);
-          if(this.ceilingChain!.length > 0){
-            for(var chain of this.ceilingChain){
-              this.sectionService
-                .query({position:chain.section_level_position,page:1,per_page:200})
-                .subscribe(
-                  (res: CustomResponse<Section[]>) =>{
-                    this.department!.push((res.data??[]));
-                  }
-                );
-            }
-          }else{
+          if(this.ceilingChain!.length <= 0){
             this.toastService.info("Please Set Ceiling Chain to Proceed ...");
             this.dialogRef.close();
+          }else{
+            const index = this.ceilingChain.findIndex(item => item.section_level_position === this.section_level_position);
+            console.log(index);
+            if(index == -1){
+              this.sectionService
+                .query({position:this.ceilingChain[0].section_level_position})
+                .subscribe(
+                  (res: CustomResponse<Section[]>) =>{
+                    this.departments = res.data??[];
+                    this.selectedDepartment = this.departments[0].id;
+                    this.sectionService.query({parent_id:this.selectedDepartment })
+                      .subscribe((res: CustomResponse<Section[]>) =>{
+                        this.costCenter = res.data??[];
+                        this.selectedCostCenter = this.costCenter[0].id
+                      });
+                  }
+                );
+            }else{
+              if(this.ceilingChain[index].next_id){
+                this.sectionService
+                  .find(this.section_id!)
+                  .subscribe(
+                    (res: CustomResponse<Section>) =>{
+                      this.departments = res.data?[res.data]:[];
+                      this.selectedDepartment = this.departments[0].id;
+                      this.sectionService.query({parent_id:this.selectedDepartment })
+                        .subscribe((res: CustomResponse<Section[]>) =>{
+                          this.costCenter = res.data??[];
+                          this.selectedCostCenter = this.costCenter[0].id;
+                        });
+                    }
+                  );
+              }else{
+                this.sectionService
+                  .find(this.section_id!)
+                  .subscribe(
+                    (res: CustomResponse<Section>) =>{
+                      this.costCenter = res.data?[res.data]:[];
+                      this.selectedCostCenter = this.costCenter[0].id;
+                      this.sectionService
+                        .find(this.costCenter[0].parent_id)
+                        .subscribe(
+                          (res: CustomResponse<Section>) =>{
+                            this.departments = res.data?[res.data]:[];
+                            this.selectedDepartment = this.departments[0].id
+                          }
+                        );
+                    }
+                  );
+              }
+            }
           }
         }
       );
+  }
+
+  getCeiling(){
+
   }
 
 }

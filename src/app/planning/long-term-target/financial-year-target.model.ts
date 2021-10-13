e7e87b667394017d2629226f8f136629ab214e92
Chrispinu Melkiory <chrismelky05@gmail.com>
2021-10-13 10:33:38 +0300

    add

--- src/app/planning/long-term-target/financial-year-target.model.ts
+++ src/app/planning/long-term-target/financial-year-target.model.ts
@@ -4,6 +4,7 @@ export class FinancialYearTarget {
     public description?: string,
     public long_term_target_id?: number,
     public financial_year_id?: number,
+    public admin_hierarchy_id?: number,
     public code?: string,
     public section_id?: number
   ) {}

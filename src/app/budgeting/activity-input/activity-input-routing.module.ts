9fd98a8f545de69c5a4f14a0b1c201578536e6a7
Chrispinu Melkiory <chrismelky05@gmail.com>
2021-10-15 14:30:24 +0300

    add

--- src/app/budgeting/activity-input/activity-input-routing.module.ts
+++ src/app/budgeting/activity-input/activity-input-routing.module.ts
@@ -6,18 +6,25 @@
  * found in the LICENSE file at https://tamisemi.go.tz/license
  */
 
-import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
-import { ActivityInputComponent } from "./activity-input.component";
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {
+  AdminHierarchyCostCentreResolveService,
+  PlanningFinancialYearResolveService,
+} from 'src/app/planning/activity/activity-routing.module';
+import { ActivityInputComponent } from './activity-input.component';
 
 const routes: Routes = [
   {
-    path: "",
+    path: ':budgetType/:adminHierarchyCostCentreId',
     component: ActivityInputComponent,
     data: {
-      defaultSort: "id:asc",
+      defaultSort: 'id:asc',
     },
-    //canActivate: [UserRouteAccessService],
+    resolve: {
+      adminHierarchyCostCentre: AdminHierarchyCostCentreResolveService,
+      financialYear: PlanningFinancialYearResolveService,
+    },
   },
 ];
 

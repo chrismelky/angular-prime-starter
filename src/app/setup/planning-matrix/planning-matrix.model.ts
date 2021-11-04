73ced7956c385581394ea80c0ca80fe2f64cdc7b
Frank Kachinga <frank.kachinga@gmail.com>
2021-11-04 09:50:14 +0300

    dev

--- src/app/setup/planning-matrix/planning-matrix.model.ts
+++ src/app/setup/planning-matrix/planning-matrix.model.ts
@@ -1,7 +1,10 @@
+import {NationalReference} from "../national-reference/national-reference.model";
+
 export class PlanningMatrix {
   constructor(
     public id?: number,
     public name?: string,
-    public national_reference_id?: number
+    public national_reference_id?: number,
+    public national_reference?: NationalReference,
   ) {}
 }
